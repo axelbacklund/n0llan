78d1a81cb850dee92bd4e0622f6304245a75b4b0
Axel Backlund <19402158+axelbacklund@users.noreply.github.com>
2021-01-25 18:29:12 +0100

    Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-typegen`,
       options: {
